5d001d06cadd530422e876cceb555dd5aed224d3
Peter Stein <peterstein@protonmail.com>
2024-01-14 01:58:36 +0100

    Extract user_bin_dir from $PATH per default

--- setup.sh
+++ setup.sh
@@ -2,7 +2,43 @@
 
 dotfiles_dir="$(pwd)"
 user_home_dir="$HOME"
-user_bin_dir="$HOME/.local/bin"
+user_bin_dir=""
+
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 [-b <user_bin_dir>]"
+    echo "Options:"
+    echo "  -b, --bin  Specify the user-specific bin directory"
+    exit 1
+}
+
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -b|--bin)
+            shift
+            user_bin_dir="$1"
+            shift
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# If user_bin_dir is not specified explicitly, use the $PATH method
+if [ -z "$user_bin_dir" ]; then
+    user_bin_dir=$(echo $PATH | tr ':' '\n' | grep '^/home/.*/bin$')
+
+    if [ $(echo "$user_bin_dir" | wc -l) -ne 1 ]; then
+        echo "Ambiguous bin path. Multiple matching paths found in \$PATH:"
+        echo "$user_bin_dir"
+        exit 1
+    fi
+
+    user_bin_dir=$(echo "$user_bin_dir" | head -n 1)
+    echo "Using bin directory found in \$PATH: $user_bin_dir"
+fi
 
 
 echo "Setting up dotfiles..."
