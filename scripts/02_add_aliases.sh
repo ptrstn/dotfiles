b3e36bff6a84cd500c6220767fa9e25d31547dc1
Peter Stein <peterstein@protonmail.com>
2024-01-15 19:57:06 +0100

    Fix already sourced check

--- scripts/02_add_aliases.sh
+++ scripts/02_add_aliases.sh
@@ -10,8 +10,8 @@ info_message "Adding aliases..."
 create_symbolic_link "$dotfiles_dir/.bash_aliases" "$HOME/.bash_aliases"
 
 # Check if .bash_aliases is sourced in .bashrc with "." (dot) or "source" command
-if grep -Eq "^(\.|\s*source)\s+~/.bash_aliases" "$user_home_dir/.bashrc"; then
-	warning_message "'.bash_aliases' is already sourced in '.bashrc.'"
+if grep -Eq "(\.|source)\s+~/.bash_aliases" "$user_home_dir/.bashrc"; then
+	warning_message "'.bash_aliases' is already sourced in '.bashrc'."
 else
 	cat templates/source_aliases.template >>"$HOME/.bashrc"
 	success_message "Added sourcing of .bash_aliases to .bashrc..."
